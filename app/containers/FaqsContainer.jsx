d23eea1a03dde216948718159b42d63b406ea90d
Anders Ramsay <anders@gmail.com>
2017-01-04 19:20:11 -0500

    loading faq data

--- app/containers/FaqsContainer.jsx
+++ app/containers/FaqsContainer.jsx
@@ -5,19 +5,24 @@ export default class FaqsContainer extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-     dataReady: false
+     dataReady: true,
+     faqs: []
     }
   }
 
   componentDidMount() {
     const url = 'https://api.realist.cloud/faqs'
+    this.setState({dataReady: false })
     fetch(url, {
       method: 'GET'
     })
     .then(response => response.json())
     .then(data => {
-      this.setState({dataReady: true })
-      console.log('data: ', data)
+      this.setState({
+        dataReady: true,
+        faqs: data.faqs
+       })
+      console.log('data: ', this.state.faqs)
     })
     .catch(function(err) {
      console.log('fetch error: ', err)
@@ -27,7 +32,7 @@ export default class FaqsContainer extends React.Component {
   render () {
     return (
       this.state.dataReady ?
-        <div>Data Ready</div>
+        <div>{this.state.faqs[0].question}</div>
       :
         <div>Loading...</div>
     )
