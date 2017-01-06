a0120aa76fb06bc1fc57df42d069fced3f3c219d
Anders Ramsay <anders@gmail.com>
2017-01-06 14:27:03 -0500

    cleanup

--- app/components/Spinner.jsx
+++ app/components/Spinner.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Grid, Row, Col } from 'react-bootstrap'
 import Loading from 'react-loading-animation'
 
 const Spinner = () =>(
@@ -7,10 +6,3 @@ const Spinner = () =>(
 )
 
 export default Spinner
-
-//
-// <Grid>
-//   <Row className="show-grid">
-//     <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-//     <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-//   </Row>
