cf7c32804baa03c8bdacdbf8e50dc3f2f3439788
Anders Ramsay <anders@gmail.com>
2017-01-07 14:55:21 -0500

    cleanup

--- app/main.js
+++ app/main.js
@@ -12,20 +12,3 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-
-// if (module.hot) {
-//   module.hot.accept('./containers/FaqsContainer', () => {
-//     // If we receive a HMR request for our App container, then reload it using require (we can't do this dynamically with import)
-//     const NextApp = require('./containers/FaqsContainer').default;
-//
-//     // And render it into the root element again
-//     render(
-//       <Provider store={store}>
-//       <AppContainer>
-//         <NextApp faqsApi={constants.faqsApi} headingText={staticContent.faqsHeader} />
-//         </AppContainer>
-//       </Provider>,
-//       rootEl
-//     );
-//   })
-// }
